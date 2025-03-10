9f4f14e286010926c353b8e69c457fa7043cea44
VicDem <vi.dematteis02@gmail.com>
2025-03-10 15:02:18 +0100

    completed logic for job_api

--- backend/data_gatherer/job_api/logic.py
+++ backend/data_gatherer/job_api/logic.py
@@ -1,26 +1,33 @@
+import csv
+import os
+from queue import Queue
+from threading import Thread
+
+from django.core.exceptions import ValidationError
 from django.db.models import Q
 from django.db.transaction import atomic
 from tqdm import tqdm
-
-
+from django.core.files import File as DjangoFile
 
 
 @atomic
 def initiate_job(job):
     from storage_api.models.data_models import UserHashtagUse, IGUser, Hashtag
     from job_api.models import Job, Task, JobIGUser, JobHashtag, JobUserHashtagUse
+    job: Job = job
+    if job.isInitiated:
+        job.isInitiating = False
+        job.save()
+        return
 
-    job:Job = job
-
-    print(f"initiating job {job}, gathering data")
     project = job.project
 
     prj_hashtags = Hashtag.objects.filter(
         project=project
-    )
+    ).order_by('content')
     prj_users = IGUser.objects.filter(
         project=project
-    )
+    ).order_by('name')
 
     for hashtag in prj_hashtags:
         uses = UserHashtagUse.objects.filter(
@@ -38,27 +45,33 @@ def initiate_job(job):
         if uses == 0:
             prj_users = prj_users.exclude(pk=user.pk)
 
-    with tqdm(total=prj_users.count(), desc="Creating job users", unit="frame") as pbar:
-        for item in prj_users:
-            user: IGUser = item
-            JobIGUser.objects.create(
-                job=job,
-                name=user.name
-            )
-            pbar.update(1)
+    create_job_users(job, prj_users)
 
     n = job.taskGranularity
     chunks = [prj_hashtags[i:i + n] for i in range(0, len(prj_hashtags), n)]
     print(f"There will be {len(chunks)} tasks, granularity: {n}")
 
-    with tqdm(total=len(chunks), desc="Creating tasks", unit="frame") as pbar:
+    create_job_tasks(chunks, job, project)
+
+    job.isInitiated = True
+    job.isInitiating = False
+    job.save()
+    print(f"completed")
+
+
+def create_job_tasks(chunks, job, project):
+    from storage_api.models.data_models import UserHashtagUse, IGUser, Hashtag
+    from job_api.models import Job, Task, JobIGUser, JobHashtag, JobUserHashtagUse
+
+    with tqdm(total=len(chunks), desc="Creating tasks", unit="task") as pbar:
         counter = 1
         for chunk in chunks:
             current_task = Task.objects.create(
                 progressiveID=counter,
                 inProgress=False,
                 isCompleted=False,
-                job=job
+                job=job,
+                totRows=len(chunk)
             )
             for item in chunk:
                 hashtag: Hashtag = item
@@ -74,7 +87,7 @@ def initiate_job(job):
                 )
 
                 job_users = JobIGUser.objects.filter(job=job)
-                with tqdm(total=queryset.count(), desc="Creating associations", unit="frame", leave=False) as pbar2:
+                with tqdm(total=queryset.count(), desc="Creating associations", unit="user", leave=False) as pbar2:
                     for j_item in queryset:
                         uhu: UserHashtagUse = j_item
                         JobUserHashtagUse.objects.create(
@@ -88,6 +101,128 @@ def initiate_job(job):
             counter = counter + 1
             pbar.update(1)
 
-    print(f"completed")
-    job.isCreated = True
+
+def create_job_users(job, prj_users):
+    from storage_api.models.data_models import UserHashtagUse, IGUser, Hashtag
+    from job_api.models import Job, Task, JobIGUser, JobHashtag, JobUserHashtagUse
+
+    with tqdm(total=prj_users.count(), desc="Creating job users", unit="user") as pbar:
+        for item in prj_users:
+            user: IGUser = item
+            JobIGUser.objects.create(
+                job=job,
+                name=user.name
+            )
+            pbar.update(1)
+
+
+def do_task(task):
+    from storage_api.models.data_models import UserHashtagUse, IGUser, Hashtag
+    from job_api.models import Job, Task, JobIGUser, JobHashtag, JobUserHashtagUse
+
+    task: Task = task
+    job: Job = task.job
+
+    if task.isCompleted:
+        print(f"Warning! Task {task} already completed")
+        return
+
+    ig_users = JobIGUser.objects.filter(job=job).order_by('name')
+    hashtags = JobHashtag.objects.filter(task=task).order_by('content')
+
+    data = [ig_users.values_list('name', flat=True)]
+
+    counter = 0
+    for hashtag in hashtags:
+        j_hashtag: JobHashtag = hashtag
+        row = [j_hashtag.content]
+        for user in ig_users:
+            j_user: JobIGUser = user
+            row.append(JobUserHashtagUse.objects.filter(
+                user=j_user,
+                hashtag=j_hashtag,
+                job=job,
+            ).count())
+        data.append(row)
+        counter = counter + 1
+        task.currentRow = counter
+        task.save()
+
+    try:
+        os.makedirs(f"./jobs_elaborations/job_{job.pk}/")
+    except FileExistsError:
+        # Defeats race condition when another thread created the path
+        pass
+
+    with open(f"./jobs_elaborations/job_{job.pk}/{job.pk}_{task.progressiveID}.csv", "w", newline='') as file:
+        writer = csv.writer(file, delimiter=";", quoting=csv.QUOTE_ALL)
+        writer.writerows(data)
+
+    with open(f"./jobs_elaborations/job_{job.pk}/{job.pk}_{task.progressiveID}.csv", "r", newline='') as file:
+        task.file = DjangoFile(file)
+        task.save()
+
+    task.inProgress = False
+    task.isCompleted = True
+    task.save()
+
+
+def do_chunks(chunks, job):
+    from job_api.models import Task, Job
+    counter = 1
+    for chunk in chunks:
+        print(f"doing chunk {counter}/{len(chunks)}")
+        threads = []
+        for task in chunk:
+            print(f"starting task {task}")
+            task: Task = task
+            task.inProgress = True
+            task.save()
+            thread = Thread(target=do_task, args=[task])
+            thread.start()
+            threads.append(thread)
+        for thread in threads:
+            thread.join()
+        print(f"chunk {counter} is done.")
+    job.isWorking = False
+    job.isCompleted = True
     job.save()
+
+
+def work_job(job, wait=False):
+
+    from job_api.models import Task, Job
+    tasks_to_be_done = Task.objects.filter(Q(job=job) & Q(isCompleted=False)).order_by('progressiveID')
+
+    if job.isInitiated is not True:
+        return ValidationError(f"The job {job} is not initiated yet! Aborted whole operation.")
+
+    n = job.threads
+    chunks = [tasks_to_be_done[i:i + n] for i in range(0, len(tasks_to_be_done), n)]
+    print(chunks)
+    print(f"There will be {len(chunks)} tasks done in parallel, threads: {n}")
+
+    job.isWorking = True
+    job.save()
+
+    thread = Thread(target=do_chunks, args=[chunks, job])
+    thread.start()
+
+    if wait:
+        thread.join()
+
+
+def calculate_whole_file(job):
+    from job_api.models import Task, Job
+    tasks = Task.objects.filter(Q(job=job)).order_by('progressiveID')
+    with open(f"./jobs_elaborations/job_{job.pk}/whole_job_{job.title}.csv", "w") as write_file:
+        for task in tasks:
+            task: Task = task
+            with open(f"./jobs_elaborations/job_{job.pk}/{job.pk}_{task.progressiveID}.csv", "r") as read_file:
+                if task.progressiveID != 1:
+                    next(read_file)
+                for line in read_file:
+                    write_file.write(line)
+    with open(f"./jobs_elaborations/job_{job.pk}/whole_job_{job.title}.csv", "r") as read_file:
+        job.file = DjangoFile(read_file)
+        job.save()
